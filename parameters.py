f016b1d08795f72c15bec13760e72d140ed5b2de
Your Name <salmansa7154@gmail.com>
2022-09-14 16:09:10 +0500

    cod

--- parameters.py
+++ parameters.py
@@ -2,5 +2,10 @@ name = "umer"
 age = 20
 f_name = "imran"
 
-print("My name is", name ,"and my father name is", f_name)
-print(age)
+# print("My name is", name ,"and my father name is", f_name)
+# print(age)
+
+
+Data=("Salman Nazir")
+x=(Data.lower("Nair.Nazeer"))
+print(x)
