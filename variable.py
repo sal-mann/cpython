2cdbe8d836420a6bf0465ac8a1114e37b569d8b9
Your Name <salmansa7154@gmail.com>
2022-09-26 14:50:31 +0500

    new

---
+++ variable.py
@@ -0,0 +1,33 @@
+# a=2
+# b=7
+# c=a+b
+# print(c)
+
+
+# a=int(input("Enter 1st no:"))
+# b=int(input("Enter 2nd no:"))
+# c=+b
+# print("Total no is",c)
+
+
+# a=int(input("Enter any no:"))
+# no=a*a
+# print("Result is",no)
+
+
+# A program to calculate simple_interest
+# input
+# Enter principle
+# Enter rate
+# Enter time
+
+## total_interest
+## total_ammount
+principal=int(input("Enter principal:"))
+rate=int(input("Enter rate:"))
+time=int(input("Enter time:"))
+simpe_interest=principal+rate+time/100
+total_ammount=simpe_interest+principal
+print("total_interset:" ,str(simpe_interest))
+print("total_ammount:" ,str(total_ammount))
+
